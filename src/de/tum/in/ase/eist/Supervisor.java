33a0005c69f6f900141313190f2d07bf5182801c
Lucas Welscher <lucas.welscher@tum.de>
2022-05-08 15:36:13 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/eist/Supervisor.java
@@ -0,0 +1,8 @@
+package de.tum.in.ase.eist;
+
+public class Supervisor {
+
+	// TODO 2: Implement the Supervisor class
+
+	// TODO 3: Implement listHierarchy() for Supervisor
+}
---
+++ src/de/tum/in/ase/eist/Worker.java
@@ -0,0 +1,7 @@
+package de.tum.in.ase.eist;
+
+public class Worker {
+	// TODO 1: Implement the Worker class
+
+	// TODO 3: Implement listHierarchy() for Worker
+}
